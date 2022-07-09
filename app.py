ba091f0602a2f2af3b5d60da7e4360e992d263a0
Vanessa Carvalho <devanessa.carvalho@gmail.com>
2022-07-09 10:48:17 -0300

    feat: criando uma receita/despesa

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
 from db import getALL, createTransaction
-from flask import Flask, render_template, redirect
-from flask.globals import request
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    result = getALL()
+  result = getALL()
+  saldo = 600
+  receita = 1000
+  despesa = 300
 
-    return render_template("index.html", result = result)
+  return render_template('index.html', saldo = saldo, receita=receita, despesa = despesa)
 
 
-@app.route("/cadastrar", methods=["POST"])
-def cadastrar():
-    description = request.form["description"]
-    value = request.form["value"]
-    type = request.form["type"]
-    date = request.form["date"]
+@app.route('/cadastrar/<tipo>', methods=['GET', 'POST'])
+def cadastrar(tipo):
+  if request.method == 'GET':
+    return render_template('cadastrar.html')
+  
+  dados = {
+    'descricao':request.form['descricao'],
+    'valor':request.form['valor'],
+    'data':request.form['data'],
+    'tipo':tipo
+  }
 
-    transaction = {
-      'description':description,
-      'value':value,
-      'type':type,
-      'date':date
-    }
-
-    createTransaction(transaction)
-    return redirect("/")
+  createTransaction(dados)
+  return redirect('/')
 
 app.run(debug=True)
